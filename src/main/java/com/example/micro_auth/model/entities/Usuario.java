de42f2b431825712092cbe11e97c71c8e628bc81
Cristopher <cr.candiam@duouc.cl>
2026-05-16 00:16:53 -0400

    primer commit

---
+++ src/main/java/com/example/micro_auth/model/entities/Usuario.java
@@ -0,0 +1,28 @@
+package com.example.micro_auth.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+    
+    @Column(nullable = false, unique = true , length = 50)
+    private String correo;
+
+    @Column(nullable = false)
+    private String contrasenia;
+
+    @Column(nullable = false, length = 50)
+    private String rol;
+}
